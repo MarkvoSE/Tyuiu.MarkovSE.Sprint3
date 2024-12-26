67a0189ed5a312c3e684ebd9eb6c2f8da7fe6eee
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 05:49:38 +0500

    Add project files.

---
+++ Tyuiu.MarkovSE.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.MarkovSE.Sprint3.Task0.V13.Lib;
+namespace Tyuiu.MarkovSE.Sprint3.Task0.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
