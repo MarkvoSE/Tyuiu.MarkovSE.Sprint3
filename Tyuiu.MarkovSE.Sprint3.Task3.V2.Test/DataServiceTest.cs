9930bbad411980a566da9794772c411b05ee6979
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 06:04:48 +0500

    Task3

---
+++ Tyuiu.MarkovSE.Sprint3.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MarkovSE.Sprint3.Task3.V2.Lib;
+namespace Tyuiu.MarkovSE.Sprint3.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "asdzzz vfvfzz v gthvz";
+            char ch = 'z';
+            int res = ds.GetMaxCharCount(str, ch);
+            Assert.IsTrue(res == 3);
+        }
+    }
+}
