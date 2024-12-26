c0f47275c9a2ffb0248b345dd802add453872eba
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 06:12:19 +0500

    Task4

---
+++ Tyuiu.MarkovSE.Sprint3.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MarkovSE.Sprint3.Task4.V26.Lib;
+namespace Tyuiu.MarkovSE.Sprint3.Task4.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startVale = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startVale, stopValue);
+
+            double wait = -1.924;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MarkovSE.Sprint3.Task4.V26/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MarkovSE.Sprint3.Task4.V26.Lib;
+namespace Tyuiu.MarkovSE.Sprint3.Task4.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Марков. С. Е. | ИСПб-24-2";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #3                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #4                                                           *");
+            Console.WriteLine("* Вариант #26                                                          *");
+            Console.WriteLine("* Выполнил: Марков С.Е.  | ИСПб-24-2                                   *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("startValue =" + startValue);
+
+            Console.WriteLine("stopValue =" + stopValue);
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Ответ = " + ds.Calculate(startValue, stopValue));
+            Console.ReadLine();
+        }
+    }
+}
